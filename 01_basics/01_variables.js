11018790c4124cf47a597f7cd2d925309afaaf76
Vidya Gayakhe <113839153+Vidya0603@users.noreply.github.com>
2024-01-20 10:15:25 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId=144553
+let accountEmail="hitesh@google.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+console.log(accountId)
+console.table([accountEmail,accountId,accountPassword,accountCity,accountId])
+// use of let is always better than var so prefer not to use var
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,15 @@
+"use strict"; //treat all JS Code as newer version
+console.log(3+3)
+
+let name="hitesh"
+let age=18
+let isLoggedIn=false
+//number=>t2 to power 53
+//bigint
+//string=>""
+//boolean=>tue/false
+//null=>standalone value
+//undefined=>
+//symbol=>unique
+//object
+console.log(typeof null);
